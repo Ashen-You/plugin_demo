dcdb3cbbc618639dc083654d848afe49f84882bd
GitHub Action <action@github.com>
2023-08-15 02:39:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/ashenyou/plugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ashenyou/plugindemo/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ashenyou.plugindemo.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ashenyou.plugindemo.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
